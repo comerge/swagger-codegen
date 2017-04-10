c5c42da78f84f77165c3f38571cc403849d7f434
Christoph Krautz <christoph.krautz@comerge.net>
2017-04-10 11:47:55 +0200

    Add simple script creating the Classic API

---
+++ generate_api.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+java -jar ./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -l typescript-angular2 -i https://tomcathosting.comerge.net/public-api-10-v2/documentation/ui/swagger.json -c ../../mono/mono-api-classic/codegen-config.json -o ../../mono/mono-api-classic/
+
+rm ../../mono/mono-api-classic/model/XmlNs0*
+
+sed -i '' '/XmlNs0/d' ../../mono/mono-api-classic/model/models.ts

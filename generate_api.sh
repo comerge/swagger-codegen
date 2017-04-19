6691e59cfd8ac78cc8d97fb4a714220406341d5c
Christoph Krautz <christoph.krautz@comerge.net>
2017-04-19 14:55:45 +0200

    Delete api and model folder before recreation

--- generate_api.sh
+++ generate_api.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf ./../mono/mono-api-classic/api ./../mono/mono-api-classic/model
+
 java -jar ./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -l typescript-angular2 -i https://tomcathosting.comerge.net/public-api-10-v2/documentation/ui/swagger.json -c ../../mono/mono-api-classic/codegen-config.json -o ../../mono/mono-api-classic/
 
 rm ../../mono/mono-api-classic/model/XmlNs0*
